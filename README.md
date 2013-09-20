23c3e6427eeb99cd19c11535155a8238d81c418b
Felix Schulze <code@felixschulze.de>
2013-09-20 09:39:40 +0200

    Update plugin for TeamCity 8.0.3

---
+++ README.md
@@ -0,0 +1,1 @@
+# TeamCity Android Emulator Plugin
--- importPackages.sh
+++ importPackages.sh
@@ -1,7 +1,19 @@
-mvn install:install-file -Dfile=server-api.jar -DgroupId=com.jetbrains.teamcity -DartifactId=server-api -Dversion=6.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=agent-api.jar -DgroupId=com.jetbrains.teamcity -DartifactId=agent-api -Dversion=6.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=common-api.jar -DgroupId=com.jetbrains.teamcity -DartifactId=common-api -Dversion=6.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=../webapps/ROOT/WEB-INF/lib/annotations.jar -DgroupId=com.jetbrains.teamcity -DartifactId=annotations -Dversion=6.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=../webapps/ROOT/WEB-INF/lib/openapi.jar -DgroupId=com.intellij -DartifactId=openapi -Dversion=6.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=../buildAgent/lib/agent.jar -DgroupId=com.jetbrains.teamcity -DartifactId=agent -Dversion=6.0.2 -Dpackaging=jar
-mvn install:install-file -Dfile=../buildAgent/lib/util.jar -DgroupId=com.intellij -DartifactId=util -Dversion=6.0.2 -Dpackaging=jar
+VERSION="8.0.3"
+set -e
+if [ ! -e TeamCity-${VERSION}.tar.gz ]; then
+	echo "Downloading TeamCity-${VERSION}.tar.gz"
+    curl -O http://download.jetbrains.com/teamcity/TeamCity-${VERSION}.tar.gz
+else
+	echo "Using TeamCity-${VERSION}.tar.gz"
+fi
+
+tar zxf TeamCity-${VERSION}.tar.gz
+mvn install:install-file -Dfile=TeamCity/devPackage/server-api.jar -DgroupId=com.jetbrains.teamcity -DartifactId=server-api -Dversion=${VERSION} -Dpackaging=jar
+mvn install:install-file -Dfile=TeamCity/devPackage/agent-api.jar -DgroupId=com.jetbrains.teamcity -DartifactId=agent-api -Dversion=${VERSION} -Dpackaging=jar
+mvn install:install-file -Dfile=TeamCity/devPackage/common-api.jar -DgroupId=com.jetbrains.teamcity -DartifactId=common-api -Dversion=${VERSION} -Dpackaging=jar
+mvn install:install-file -Dfile=TeamCity/webapps/ROOT/WEB-INF/lib/annotations.jar -DgroupId=com.jetbrains.teamcity -DartifactId=annotations -Dversion=${VERSION} -Dpackaging=jar
+mvn install:install-file -Dfile=TeamCity/webapps/ROOT/WEB-INF/lib/openapi.jar -DgroupId=com.intellij -DartifactId=openapi -Dversion=${VERSION} -Dpackaging=jar
+mvn install:install-file -Dfile=TeamCity/buildAgent/lib/agent.jar -DgroupId=com.jetbrains.teamcity -DartifactId=agent -Dversion${VERSION} -Dpackaging=jar
+mvn install:install-file -Dfile=TeamCity/buildAgent/lib/util.jar -DgroupId=com.intellij -DartifactId=util -Dversion=${VERSION} -Dpackaging=jar
+
+
